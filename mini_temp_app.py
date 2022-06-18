4c36423268a354e6bb4411de459ff647eb33418a
HendrikKlug-synthara <hendrik.klug@synthara.ai>
2022-06-18 10:46:51 +0200

    get values from hass

--- mini_temp_app.py
+++ mini_temp_app.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import numpy as np
 from flask import Flask, Response
 from modun.file_io import json2dict
-from pymongo import MongoClient
+from requests import get
 
 app = Flask(__name__)
 
@@ -14,30 +14,37 @@ def get_values_from_db(collection):
             return elem
 
 
+def get_sensor1_values(api_token: str) -> dict:
+    return_dict = {}
+    for val in ['temp', 'humidity']:
+        url = f"http://localhost:8123/api/states/sensor.{val}"
+        headers = {
+            "Authorization": f"Bearer {api_token}",
+            "content-type": "application/json",
+        }
+
+        response = get(url, headers=headers).json()
+        return_dict[f"{val}"] = response['state'] + response['attributes']["unit_of_measurement"]
+        return_dict[f"{val}_last_update"] = response['last_updated']
+
+    return return_dict
+
+
 @app.route("/")
 def read_temp():
     config = json2dict(Path('~/.config/burchen_db.json').expanduser())
-    client = MongoClient(config['mongo_uri'])
-    db = client.ppb
-    inkbird1_collection = db.inkbird1
-    HA_sensor1_collection = db.HA_sensor1
 
-    inkbird1_values = get_values_from_db(inkbird1_collection)
-    HA_sensor1_values = get_values_from_db(HA_sensor1_collection)
+    sensor1_values = get_sensor1_values(config["hass_api_token"])
 
-    if not inkbird1_values:
+    if not sensor1_values:
         return Response(status=500)
     return f'<html>' \
            f'<h1>Ob der Baechi</h1>' \
            f'<br>' \
            f'<b>Sensor 1 </b><br>' \
-           f'Temperature: {inkbird1_values["temp"]} °C<br>' \
-           f'Humidity: {inkbird1_values["humidity"]}%<br>' \
-           f'<i>Last measure: {inkbird1_values["date"]}</i><br>' \
-           f'<b>Sensor 2 </b><br>' \
-           f'Temperature: {HA_sensor1_values["temp"]} °C<br>' \
-           f'Humidity: {HA_sensor1_values["humidity"]}%<br> ' \
-           f'<i>Last measure: {HA_sensor1_values["date"]}</i><br>' \
+           f'Temperature: {sensor1_values["temp"]}<br>' \
+           f'Humidity: {sensor1_values["humidity"]}%<br>' \
+           f'<i>Last measure: {sensor1_values["temp_last_update"]}</i><br>' \
            f'<div width:100vh height:100vh>' \
            f'<img src="https://github.com/JulianKlug/TemperatureSensorApp/raw/main/ceyna.png" alt="Ceyna"' \
            f'style="padding: 0 5px 10px 10px; position: absolute; bottom: 0; right: 0;">' \
