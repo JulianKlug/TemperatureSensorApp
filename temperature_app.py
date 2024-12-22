a433da7aea098dd553c94600230656c6e9aead2d
Hendrik Klug <hendrik.klug@gmail.com>
2024-12-22 14:16:29 +0100

    disable debug mode in temperature app server

--- temperature_app.py
+++ temperature_app.py
@@ -426,4 +426,4 @@ def handle_data():
 
 
 if __name__ == "__main__":
-    app.run_server(debug=True, host="0.0.0.0", port=CONFIG.flask_port)
+    app.run_server(debug=False, host="0.0.0.0", port=CONFIG.flask_port)
